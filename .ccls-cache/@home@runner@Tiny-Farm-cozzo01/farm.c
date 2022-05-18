658baead13e521f3044204adbca11d71b14f9ae0
Leonardo <81624693+cozzoNbhd@users.noreply.github.com>
2022-05-18 14:24:03 +0000

    Tentativi per far funzionare getopt(). Non sono ancora riuscito ad utilizzarla correttamente

--- .ccls-cache/@home@runner@Tiny-Farm-cozzo01/farm.c
+++ .ccls-cache/@home@runner@Tiny-Farm-cozzo01/farm.c
@@ -1,10 +1,41 @@
+#include "xerrori.h"
 
 int main(int argc, char *argv[]) {
-  // controlla numero argomenti
-  if(argc<2) {
+	
+	// controlla numero argomenti
+  if (argc < 2) {
 		printf("Uso: %s file [file ...] \n",argv[0]);
 		return 1;
   }
+
+	int nthreadflag = 0;
+  int qlenflag = 0;
+	int delatyflag = 0;
+
+	int nthread = 0;
+	int qlen = 0;
+	int delay = 0;
+
+  int c;
+	char *value;
+
+
+  while ((c = getopt (argc, argv, value)) != -1) {
+    switch (c) {
+      case 'n':
+        nthreadflag = 1;
+				printf("%s", value);
+        break;
+      case 'q':
+        qlenflag = 1;
+				printf("%s", value);
+        break;
+      case 't':
+        delatyflag = 0;
+				printf("%s", value);
+        break;
+		}
+	}
 	
 	return 0;
 }
---
+++ makefile
@@ -0,0 +1,23 @@
+# definizione del compilatore e dei flag di compilazione
+# che vengono usate dalle regole implicite
+CC=gcc
+CFLAGS=-g -Wall -O -std=c99 -DMAX_SIZE=4096
+LDLIBS=-lm -lrt -pthread
+
+# eseguibili da costruire
+EXECS=farm
+
+.PHONY: clean
+
+all: $(EXECS)
+
+# dipendenze
+farm: farm.o xerrori.o
+
+# target che cancella eseguibili e file oggetto
+clean:
+	rm -f $(EXECS) *.o  *.zip
+
+# target che crea l'archivio dei sorgenti
+zip:
+	zip farm.zip makefile *.c *.h
